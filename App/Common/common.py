8927d60378faa45be6ff8bca2286e9f8df3d46d5
tongyu110 <pengchengxue110@163.com>
2019-11-29 23:56:56 +0800

    'Doc'

--- App/Common/common.py
+++ App/Common/common.py
@@ -1,3 +1,5 @@
+import os
+import copy
 def sortpost(lis):
 	"""冒泡排序"""
 	for i in range(len(lis)-1):
@@ -20,8 +22,37 @@ def calc(*number):
 		total = total + n*n
 	return total
 
+def fac():
+	total = 1
+	num = int(input("请输入一个数字"))
+	if num < 0:
+		input("抱歉，负数没有乘阶")
+	elif num == 0:
+		input("0 的乘阶为 1")
+	else:
+		for i in range(1,num+1):
+			total = total * i
+	return total
 
+def printDirFileName(path='.'):
+	for d in os.listdir(path):
+		print(d)
+
+def strToLower(str):
+	return str.lower()
+
+def test1(x,y):
+	x = y-x
+	y = y-x
+	x = y+x
+	return {'x':x,'y':y}
+
+def test():
+	global Money
+	Money = Money + 1
+
+Money = 10
 
 if __name__ == '__main__':
-	re = calc(10)
-	print(re)
+	test()
+	print(Money)
